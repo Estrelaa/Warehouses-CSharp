1e20986a5d1cb2a04a6b04551f8a185c2938191b
Kieran Winfield <K.Winfield@ucas.ac.uk>
2020-01-16 10:59:42 +0000

    Updated SQL file with updated changes to the database

--- RunMeInDeploymentServer!.sql
+++ RunMeInDeploymentServer!.sql
@@ -3,3 +3,7 @@ CREATE INDEX nameAtThisWarehouse ON em(name, w_id);
 CREATE INDEX productComanyLink ON gtin(p_id, gcp_cd);
 CREATE INDEX ProductIDAndNAme ON gtin(p_id, gcd_cd);
 CREATE INDEX ProductToWarehouse ON stock(w_id, p_id);
+
+ALTER TABLE em
+DROP CONSTRAINT em_pkey,
+ADD personal_ID SERIAL NOT NULl PRIMARY KEY;
