e06d01cb4c1fab7b483378159ebd2dee42ddd9ea
Kieran Winfield <K.Winfield@ucas.ac.uk>
2020-01-17 10:39:02 +0000

    Changed formatting of constructs to be consistant, updated formating to current C# standard

--- ShipIt/Models/DataModels/EmployeeDataModel.cs
+++ ShipIt/Models/DataModels/EmployeeDataModel.cs
@@ -18,26 +18,44 @@ namespace ShipIt.Models.DataModels
         public int PersonalId { get; set; }
 
         public EmployeeDataModel(IDataReader dataReader) : base(dataReader)
-        { }
+        { 
+        }
 
         public EmployeeDataModel()
-        { }
+        { 
+        }
 
         public EmployeeDataModel(Employee employee)
         {
-            this.Name = employee.Name;
-            this.WarehouseId = employee.WarehouseId;
-            this.Role = MapApiRoleToDatabaseRole(employee.role);
-            this.Ext = employee.Ext;
-            this.PersonalId = employee.PersonalId;
+            Name = employee.Name;
+            WarehouseId = employee.WarehouseId;
+            Role = MapApiRoleToDatabaseRole(employee.role);
+            Ext = employee.Ext;
+            PersonalId = employee.PersonalId;
         }
 
         private string MapApiRoleToDatabaseRole(EmployeeRole employeeRole)
         {
-            if (employeeRole == EmployeeRole.CLEANER) return DataBaseRoles.Cleaner;
-            if (employeeRole == EmployeeRole.MANAGER) return DataBaseRoles.Manager;
-            if (employeeRole == EmployeeRole.OPERATIONS_MANAGER) return DataBaseRoles.OperationsManager;
-            if (employeeRole == EmployeeRole.PICKER) return DataBaseRoles.Picker;
+            if (employeeRole == EmployeeRole.CLEANER)
+            {
+                return DataBaseRoles.Cleaner;
+            }
+
+            if (employeeRole == EmployeeRole.MANAGER)
+            {
+                return DataBaseRoles.Manager;
+            }
+
+            if (employeeRole == EmployeeRole.OPERATIONS_MANAGER)
+            {
+                return DataBaseRoles.OperationsManager;
+            }
+
+            if (employeeRole == EmployeeRole.PICKER)
+            {
+                return DataBaseRoles.Picker;
+            }
+
             throw new ArgumentOutOfRangeException("EmployeeRole");
         }
     }
