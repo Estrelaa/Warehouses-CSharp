f26ba509d55ed802be2bdce9fafef5c6d7507cef
Kieran Winfield <K.Winfield@ucas.ac.uk>
2020-01-17 10:27:22 +0000

    Got rid of unsed using tags and a blank line

--- ShipIt/Models/ApiModels/RemoveEmployeeRequest.cs
+++ ShipIt/Models/ApiModels/RemoveEmployeeRequest.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace ShipIt.Models.ApiModels
+﻿namespace ShipIt.Models.ApiModels
 {
     public class RemoveEmployeeRequest
     {
         public string Name { get; set; }
         public int PersonalID { get; set; }
-
     }
 }
